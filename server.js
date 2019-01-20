2c40b3a350535f371232b63d86a32f9fad58cf67
Aleshkevich, Kirill <k.aleshkevich@itransition.com>
2019-01-20 19:49:50 +0300

    some changes for heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
+const dist = path.join(__dirname, '/dist');
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(dist));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
